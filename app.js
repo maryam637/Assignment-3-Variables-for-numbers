bfaac1f641bebf9a328784223d3845c2f166a823
Mariyam <farooqmaryam508@gmail.com>
2025-07-06 06:34:25 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var age = "I am 18 years old"
+   alert(age);
+
+  var site = "You have visit this site many time"
+   alert(site);
+
+var birthyear = ["My birth years is 2006" , "<br>"];
+var data = "Data type decalared variable is nummber ";
+document.write(birthyear)
+document.write(data)
